deee94179263d4769d27c90175867c7bb47a19f4
pedrolgois <pedro_gois@hotmail.com>
2023-12-22 10:29:37 -0300

    testes

--- src/tests/api/leilao.test.ts
+++ src/tests/api/leilao.test.ts
@@ -1,5 +1,24 @@
-test('Criação de leilão', () => {
-  const name = 'Item1';
-  const description = 'Descricao Item1';
-  const minimumValue = 1000;
+import itens from "../../app/api/itens/route";
+
+test('Espera criar um leilão normalmente', () => {
+  const req = {
+    body: JSON.stringify({
+       name : 'Item1',
+       description : 'Descricao Item1',
+       minimumValue : 1000,
+    })
+  }
+
+  const json = jest.fn();
+  const status = jest.fn(()=> {
+    return(
+      json
+    )
+  })
+  const res = { status }
+
+  itens.POST(req, res);
+
+  console.log(json.mock)
+
 })
